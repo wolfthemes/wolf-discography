a3aef8782d9160de58cf4e4d5f2d549385bc17d0
Constantin Saguin <contact@wolfthemes.com>
2025-09-14 18:48:55 +0200

    feat: added default content

---
+++ Functions/Frontend/AttributeProcessor.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Attribute Processor
+ *
+ * @package WolfDiscography
+ * @subpackage Frontend
+ * @since 2.0.0
+ */
+
+namespace WolfDiscography\Frontend;
+
+defined( 'ABSPATH' ) || exit;
+
+class AttributeProcessor {
+
+}
---
+++ Functions/Frontend/PostOutputFactory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Post Output Factory
+ *
+ * @package WolfDiscography
+ * @subpackage Frontend
+ * @since 2.0.0
+ */
+
+namespace WolfDiscography\Frontend;
+
+defined( 'ABSPATH' ) || exit;
+
+class PostOutputFactory {
+
+}

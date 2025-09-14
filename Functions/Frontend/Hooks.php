a81a5394817a97eb411bdad717bcfbcf2e218985
Constantin Saguin <contact@wolfthemes.com>
2025-09-14 13:17:20 +0200

    fix: release metadata hook

--- Functions/Frontend/Hooks.php
+++ Functions/Frontend/Hooks.php
@@ -33,7 +33,7 @@ class Hooks {
 		add_action( 'get_the_generator_html', array( $this, 'generator_tag' ), 10, 2 );
 		add_action( 'get_the_generator_xhtml', array( $this, 'generator_tag' ), 10, 2 );
 
-		add_action( 'wolf_release_start', array( $this, 'wd_release_microdata' ) );
+		add_action( 'wolf_release_start', array( $this, 'release_microdata' ) );
 
 		/**
 		 * Content wrappers
--- templates/content-grid.php
+++ templates/content-grid.php
@@ -8,6 +8,10 @@
  * @since 1.0.2
  */
 
+use WolfDiscography\Frontend\Helpers;
+
+defined( 'ABSPATH' ) || exit;
+
 extract(
 	wp_parse_args(
 		$template_args,
@@ -33,13 +37,13 @@ $text_style         = '';
 $overlay_text_color = apply_filters( 'wd_release_overlay_text_color', $overlay_text_color );
 
 if ( $overlay_text_color && 'overlay' === $layout ) {
-	$text_color = wd_convert_color_class_to_hex_value( $overlay_text_color, $overlay_text_custom_color );
+	$text_color = Helpers::convert_color_class_to_hex_value( $overlay_text_color, $overlay_text_custom_color );
 	if ( $text_color ) {
-		$text_style .= 'color:' . wd_sanitize_color( $text_color ) . '!important;';
+		$text_style .= 'color:' . Helpers::sanitize_color( $text_color ) . '!important;';
 	}
 }
 
-$dominant_color       = wd_get_image_dominant_color( get_post_thumbnail_id() );
+$dominant_color       = Helpers::get_image_dominant_color( get_post_thumbnail_id() );
 $actual_overlay_color = '';
 
 if ( 'auto' === $overlay_color ) {
@@ -47,15 +51,15 @@ if ( 'auto' === $overlay_color ) {
 	$actual_overlay_color = $dominant_color;
 
 } else {
-	$actual_overlay_color = wd_convert_color_class_to_hex_value( $overlay_color, $overlay_custom_color );
+	$actual_overlay_color = Helpers::convert_color_class_to_hex_value( $overlay_color, $overlay_custom_color );
 }
 
-$overlay_tone_class   = 'overlay-tone-' . wd_get_color_tone( $actual_overlay_color );
+$overlay_tone_class   = 'overlay-tone-' . Helpers::get_color_tone( $actual_overlay_color );
 $custom_redirect_link = get_post_meta( get_the_ID(), '_release_redirect_url', true );
 $permalink            = ( $custom_redirect_link ) ? $custom_redirect_link : get_the_permalink();
 $target               = ( $release_do_redirect_url && $custom_redirect_link ) ? '_blank' : '';
 ?>
-<article <?php wd_post_attr( array( $overlay_tone_class ) ); ?>>
+<article <?php Helpers::post_attr( array( $overlay_tone_class ) ); ?>>
 	<?php
 		/**
 		 * wolf_release_start_hook
@@ -67,14 +71,14 @@ $target               = ( $release_do_redirect_url && $custom_redirect_link ) ? 
 			<a class="entry-link-mask" target="<?php echo esc_attr( $target ); ?>" href="<?php echo esc_url( $permalink ); ?>"></a>
 			<?php
 				$style              = '';
-				$img_dominant_color = wd_get_image_dominant_color( get_post_thumbnail_id() );
+				$img_dominant_color = Helpers::get_image_dominant_color( get_post_thumbnail_id() );
 
 			if ( $img_dominant_color ) {
-				$img_dominant_color = wd_sanitize_color( $img_dominant_color );
+				$img_dominant_color = Helpers::sanitize_color( $img_dominant_color );
 				$style              = "background-color:$img_dominant_color;";
 			}
 			?>
-				<div class="entry-image" style="<?php echo wd_esc_style_attr( $style ); ?>">
+				<div class="entry-image" style="<?php echo Helpers::esc_style_attr( $style ); ?>">
 				<?php
 
 					wd_resized_thumbnail( $custom_thumbnail_size ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped,Generic.Files.EndFileNewline.NotFound
@@ -82,7 +86,7 @@ $target               = ( $release_do_redirect_url && $custom_redirect_link ) ? 
 			</div>
 			<div class="entry-inner">
 				<?php
-					$dominant_color = wd_get_image_dominant_color( get_post_thumbnail_id() );
+					$dominant_color = Helpers::get_image_dominant_color( get_post_thumbnail_id() );
 
 				if ( $dominant_color && 'auto' === $overlay_color ) {
 					$overlay_custom_color = $dominant_color;
@@ -96,13 +100,13 @@ $target               = ( $release_do_redirect_url && $custom_redirect_link ) ? 
 						)
 					);
 
-					if ( wd_is_elementor_editor() ) {
+					if ( Helpers::is_elementor_editor() ) {
 						$text_style = '';
 					}
 					?>
-				<div style="<?php echo wd_esc_style_attr( $text_style ); ?>" class="entry-summary">
+				<div style="<?php echo Helpers::esc_style_attr( $text_style ); ?>" class="entry-summary">
 					<h3 class="entry-title"><a target="<?php echo esc_attr( $target ); ?>"  href="<?php echo esc_url( $permalink ); ?>" style="<?php echo wd_esc_style_attr( $text_style ); ?>"><?php the_title(); ?></a></h3>
-					<div style="<?php echo wd_esc_style_attr( $text_style ); ?>" class="entry-taxonomy">
+					<div style="<?php echo Helpers::esc_style_attr( $text_style ); ?>" class="entry-taxonomy">
 						<?php echo get_the_term_list( get_the_ID(), 'band', apply_filters( 'wd_release_tax_before', '' ), ' / ', '' ); ?>
 					</div><!-- .entry-taxonomy -->
 					<?php do_action( 'wd_loop_release_caption_end' ); ?>

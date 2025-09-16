f733f9b0f9fa908cf8fe83b8ea6415778e36fd8a
Constantin Saguin <wolfthemes14@gmail.com>
2025-09-16 12:25:21 +0200

    feat: created TemplateHelper class

---
+++ Functions/Frontend/TemplateHelper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Helper Class
+ *
+ * @package WolfDiscography/Frontend
+ * @since 1.6.0
+ */
+
+namespace WolfDiscography\Frontend;
+
+use WolfDiscography\Frontend\Helpers;
+
+defined( 'ABSPATH' ) || exit;
+
+class Template_Helper {
+
+}
